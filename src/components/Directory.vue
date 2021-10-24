3bc72dd8a61cbd1803f598b013fe9c3106082690
Denis <dnkhisamiev@gmail.com>
2021-10-24 19:29:34 +0300

    Updated tree logic

--- src/components/Directory.vue
+++ src/components/Directory.vue
@@ -1,10 +1,36 @@
 <template>
-
+  <div>
+    <h1 @click="show = !show">{{node.name}}</h1>
+    <div v-if="hasChildren && show">
+      <directory
+          v-for="child in node.contents"
+          :key="child.name"
+          :node="child"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "Directory.vue"
+  name: "Directory",
+  props: {
+    node: {
+      type: Object,
+      required: true
+    }
+  },
+  data () {
+    return {
+      show: false
+    }
+  },
+  computed: {
+    hasChildren () {
+      const { contents } = this.node
+      return contents && contents.length > 0
+    }
+  }
 }
 </script>
 
