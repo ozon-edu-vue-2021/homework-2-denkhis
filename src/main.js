f85ac65778678580290df5d45d52da89d7b4ea2d
Denis <dnkhisamiev@gmail.com>
2021-10-26 11:59:41 +0300

    Added Vuex

--- src/main.js
+++ src/main.js
@@ -1,9 +1,23 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App.vue'
 import './assets/styles/main.css'
 
 Vue.config.productionTip = false
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  state: {
+    filePath: ''
+  },
+  mutations: {
+    setFilePath (state, payload) {
+      state.filePath = payload
+    }
+  }
+})
 
 new Vue({
-  render: h => h(App),
+  store,
+  render: h => h(App)
 }).$mount('#app')
